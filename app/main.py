8cbab8e46008ad47fdc96472f689a8c5465e8230
a-kobori <89172056+a-kobori@users.noreply.github.com>
2023-07-26 21:27:41 +0900

    spotipyライブラリを利用

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ from spotipy.oauth2 import SpotifyOAuth
 import urllib
 import os
 
+app = FastAPI()
+
 # Spotify Developer Dashboardで取得した情報を入力します
 CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
@@ -15,13 +17,14 @@ REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 # 最近聴いた曲を取得するためには'user-read-recently-played'が必要です
 SCOPE = 'user-read-recently-played'
 
-app = FastAPI()
+sp_oauth = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
+                        redirect_uri=REDIRECT_URI, scope=SCOPE)
 
 
 @app.get("/get_recently_played")
 async def get_recently_played():
     # ユーザーをSpotifyの認証ページにリダイレクト
-    auth_url = f'https://accounts.spotify.com/authorize?response_type=code&client_id={CLIENT_ID}&scope={urllib.parse.quote(SCOPE)}&redirect_uri={urllib.parse.quote(REDIRECT_URI)}'
+    auth_url = sp_oauth.get_authorize_url()
     return Response(headers={"Location": auth_url}, status_code=303)
 
 
@@ -31,26 +34,10 @@ async def callback(request: Request):
     code = request.query_params.get('code')
 
     # 認証コードからアクセストークンを取得
-    auth_header = base64.b64encode(
-        f'{CLIENT_ID}:{CLIENT_SECRET}'.encode('utf-8')).decode('utf-8')
-    headers = {
-        'Authorization': f'Basic {auth_header}',
-    }
-    data = {
-        'grant_type': 'authorization_code',
-        'code': code,
-        'redirect_uri': REDIRECT_URI,
-    }
-    response = requests.post(
-        'https://accounts.spotify.com/api/token', headers=headers, data=data)
-    access_token = response.json().get('access_token')
+    token_info = sp_oauth.get_access_token(code)
 
     # アクセストークンを使用して最近聴いた曲の履歴を取得
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-    }
-    response = requests.get(
-        'https://api.spotify.com/v1/me/player/recently-played', headers=headers)
-    recently_played = response.json()
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    recently_played = sp.current_user_recently_played()
 
     return recently_played
