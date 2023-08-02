4b0cc70be8b448c33da86116f2fbf1023e34da4d
a-kobori <89172056+a-kobori@users.noreply.github.com>
2023-08-02 16:25:06 +0900

    ワークフローの簡単なミスを修正

--- .github/workflows/scripts/start_docker_container.sh
+++ .github/workflows/scripts/start_docker_container.sh
@@ -6,13 +6,13 @@ SPOTIFY_CLIENT_SECRET=$2
 NOTION_API_TOKEN=$3
 ACCESS_TOKEN=$4
 
-cd ~/spotify-api
-docker rm -f spotify-api || true
-docker build -t spotify-api -f ./docker/api/Dockerfile .
-docker run -d --name spotify-api \
+cd ~/daily-api
+docker rm -f daily-api || true
+docker build -t daily-api -f ./docker/api/Dockerfile .
+docker run -d --name daily-api \
   -p 80:8080 \
   -e SPOTIFY_CLIENT_ID=$SPOTIFY_CLIENT_ID \
   -e SPOTIFY_CLIENT_SECRET=$SPOTIFY_CLIENT_SECRET \
-  -e NOTION_API_TOKEN=$NOTION_API_TOKEN
-  -e ACCESS_TOKEN=$ACCESS_TOKEN
-  spotify-api
+  -e NOTION_API_TOKEN=$NOTION_API_TOKEN \
+  -e ACCESS_TOKEN=$ACCESS_TOKEN \
+  daily-api
