c62ff7b505bd0111150d7ad854c4a0e47a2e6e81
a-kobori <89172056+a-kobori@users.noreply.github.com>
2023-08-21 22:20:30 +0900

    テキストを受け取れるようにする

--- app/router/line.py
+++ app/router/line.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter
 from app.line.line_client import LineClientFactory
+from pydantic import BaseModel
 
 router = APIRouter()
 line_client = LineClientFactory.get_instance()
 
 
+class Text(BaseModel):
+    content: str
+
+
 @router.post("/message", response_model=dict)
-def message_push():
-    return line_client.push_message("hello")
+def message_push(text: Text):
+    return line_client.push_message(text.content)
