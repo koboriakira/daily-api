734ebd332a53d86917a6accf20d8c8f19916a33d
a-kobori <89172056+a-kobori@users.noreply.github.com>
2023-08-21 22:17:35 +0900

    afterを除去

--- app/router/spotify/current_user_recently_played.py
+++ app/router/spotify/current_user_recently_played.py
@@ -6,8 +6,8 @@ router = APIRouter()
 
 
 @router.get("/", response_model=list[RecentlyPlayedTrack])
-async def current_user_recently_played(after: Optional[float] = None) -> list[RecentlyPlayedTrack]:
+async def current_user_recently_played() -> list[RecentlyPlayedTrack]:
     """ 最近聴いている曲の一覧を取得 """
     spotify_controller = SpotifyController.get_instance()
-    result = spotify_controller.current_user_recently_played(after=after)
+    result = spotify_controller.current_user_recently_played()
     return result
