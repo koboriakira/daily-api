30ffe959950864518a42f5958c2862568c685f6f
a-kobori <89172056+a-kobori@users.noreply.github.com>
2023-08-28 16:28:18 +0900

    適切にスケジュールをとれるよう改良

--- app/router/gas.py
+++ app/router/gas.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from app.line.line_client import LineClientFactory
-from pydantic import BaseModel
 from datetime import date as DateObject
 from datetime import datetime as DateTimeObject
 from datetime import timedelta
@@ -14,26 +13,30 @@ router = APIRouter()
 line_client = LineClientFactory.get_instance()
 
 
-class Date(BaseModel):
-    value: DateObject
+@router.get("/calendar/", response_model=list[dict])
+def get_calendar(start_date: DateObject, end_date: DateObject):
 
+    # UTC+00:00で検索してしまうため、ちょっと広めに検索して、あとで絞る
+    replaced_start_date = start_date - timedelta(days=1)
 
-@router.get("/calendar/{date}", response_model=list[dict])
-def get_calendar(date: DateObject):
-    # ex. /calendar/2023-08-28
-    url = f"{GAS_CALENDAR_API_URI}?date={date}"
+    url = f"{GAS_CALENDAR_API_URI}?startDate={replaced_start_date}&endDate={end_date}"
     response = requests.get(url)
     data = json.loads(response.text)
 
-    def convert(schedule: dict) -> dict:
-        start = DateTimeObject.fromisoformat(
-            schedule["start"]) + timedelta(hours=9)
-        end = DateTimeObject.fromisoformat(
-            schedule["end"]) + timedelta(hours=9)
-        return {
-            "start": start.isoformat(),
-            "end": end.isoformat(),
-            "title": schedule["title"],
-        }
+    def convert(data: list[dict], start_date: DateTimeObject, end_date: DateTimeObject):
+        for schedule in data:
+            start = DateTimeObject.fromisoformat(
+                schedule["start"]) + timedelta(hours=9)
+            end = DateTimeObject.fromisoformat(
+                schedule["end"]) + timedelta(hours=9)
+            if start_date.timestamp() <= start.timestamp() and end.timestamp() <= end_date.timestamp():
+                yield {
+                    "start": start.isoformat(),
+                    "end": end.isoformat(),
+                    "title": schedule["title"],
+                }
 
-    return [convert(schedule) for schedule in data]
+    return list(convert(data,
+                        DateTimeObject(start_date.year,
+                                       start_date.month, start_date.day),
+                        DateTimeObject(end_date.year, end_date.month, end_date.day)))
