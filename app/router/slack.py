714c5c2debfe82c0ace603bf4c5f90cbc73f3a11
a-kobori <89172056+a-kobori@users.noreply.github.com>
2023-08-31 20:45:35 +0900

    SlackのファイルアップロードAPIのラッパーを作成

--- app/router/slack.py
+++ app/router/slack.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, File, UploadFile
 from pydantic import BaseModel
 from slack_sdk import WebClient
-import shutil
 import os
 
+DIARY = "C05F6AASERZ"
+TEST = "C05H3USHAJU"
+
 router = APIRouter()
-slack_bot = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
 slack_user = WebClient(token=os.environ["SLACK_USER_TOKEN"])
 
 
@@ -17,6 +18,11 @@ class Text(BaseModel):
 def message_push(file: UploadFile = File(...)):
     if file:
         filename = file.filename
-        # fileobj = file.file
+        slack_user.files_upload_v2(
+            channels=TEST,
+            initial_comment="アップロードテスト",
+            file=file.file,
+            filename=filename,
+        )
         return {"アップロードファイル名": filename}
     return {"Error": "アップロードファイルが見つかりません。"}
