58e0cae32fb05bf72a50de469e2583e37decdb1e
a-kobori <89172056+a-kobori@users.noreply.github.com>
2023-08-31 19:27:17 +0900

    APIを追加

--- app/router/line.py
+++ app/router/line.py
@@ -13,3 +13,10 @@ class Text(BaseModel):
 @router.post("/message", response_model=dict)
 def message_push(text: Text):
     return line_client.push_message(text.content)
+
+
+@router.post("/message/photo",
+             response_model=dict,
+             description="今日撮影した写真があることをLINE通知します")
+def message_push_photo():
+    return line_client.push_message("今日撮影した写真があるので管理しましょう！")
