d7e76343aaf84a977ef23c73fa32187f6b23a2d3
a-kobori <89172056+a-kobori@users.noreply.github.com>
2023-09-10 00:01:43 +0900

    URIのかぶりがなくなるように修正

--- app/router/spotify/track.py
+++ app/router/spotify/track.py
@@ -9,30 +9,6 @@ from typing import Optional
 router = APIRouter()
 
 
-@router.post("/{track_id}/add_notion", response_model=NotionUrl)
-async def add_track_to_notion(track_id: str):
-    """
-    指定されたSpotifyの曲もしくはアルバムをNotionに追加する。
-    作成したページのURLを返す。
-    """
-    spotify_controller = SpotifyController.get_instance()
-    notion_client = NotionClient()
-    track = spotify_controller.get_track(track_id=track_id)
-    if track is not None:
-        daily_log = notion_client.get_daily_log()
-        daily_log_id = daily_log.id
-        result = notion_client.add_track(
-            track=track, daily_log_id=daily_log_id)
-        return NotionUrl(url=result["url"], block_id=result["id"])
-    album = spotify_controller.get_album(album_id=track_id)
-    if album is not None:
-        daily_log = notion_client.get_daily_log()
-        daily_log_id = daily_log.id
-        result = notion_client.add_album(
-            album=album, daily_log_id=daily_log_id)
-        return NotionUrl(url=result["url"], block_id=result["id"])
-
-
 @router.get("/{track_id}", response_model=TrackEntity)
 async def get_track(track_id: str):
     """
@@ -52,7 +28,7 @@ async def recommend(track_id: str):
     return spotify_controller.recommend(track_id=track_id)
 
 
-@router.get("/playing", response_model=Optional[TrackEntity])
+@router.get("/current/playing", response_model=Optional[TrackEntity])
 async def get_playing():
     """
     現在流れている曲を取得する
